14e61f105ee664fa50e7e7326e390ad9fc364ffb
daksh890 <57205074+daksh890@users.noreply.github.com>
2023-03-09 17:48:29 +0530

    Some changes.

--- src/Components/Top/top.js
+++ src/Components/Top/top.js
@@ -6,12 +6,13 @@ function Top() {
     <div className="top">
       <div className="letter">
         <div className="heading">
-          <h1>Double the hires, half the effort</h1>
+          <h1>Double the Money, half the effort</h1>
         </div>
         <div className="paragraph">
           <p>
             Open conversations and nurture relationships at scale and be the
-            first choice when your ideal candidate is ready to explore.
+            first choice when your ideal candidate is ready to explore. Here are
+            some changes to Look for immediately.
           </p>
         </div>
         <div className="button">
